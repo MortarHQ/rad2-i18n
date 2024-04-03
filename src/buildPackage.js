a3b6d368aa9bc2b27019167310840c6c184cca55
landminehq <z1216198007@gmail.com>
2024-04-03 20:03:03 +0800

    Update buildPackage.js now it will support manual lang, auto lang, origin lang

--- src/buildPackage.js
+++ src/buildPackage.js
@@ -1,23 +1,65 @@
 // 创建一个zip压缩包，为了将目标文件打包到指定路径
 // 文件夹: src/ftbqkeys/backup --> package.zip中backup
-// 文件夹：src/ftbqkeys/kubejs --> package.zip中kubejs
 // 文件夹：src/ftbqkeys/config/ftbquests --> package.zip中config/ftbquests/quests
+// 文件: src/ftbqkeys/kubejs/assets/kubejs/lang/en_us.json --> package.zip中kubejs/assets/kubejs/lang/en_us.json
+// 文件：处理完毕的zh_cn.json --> package.zip中kubejs/assets/kubejs/lang/zh_cn.json
+
+import fs from "fs";
+import archiver from "archiver";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// 当前文件路径与文件夹路径
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // 文件夹路径和目标路径的数组
 const directories = [
   { path: "src/ftbqkeys/backup/", target: "backup" },
-  { path: "src/ftbqkeys/kubejs/", target: "kubejs" },
   { path: "src/ftbqkeys/config/ftbquests/", target: "config/ftbquests/quests" },
 ];
 
-import fs from "fs";
-import archiver from "archiver";
+// 文件路径和目标路径的数组
+const files = [
+  {
+    file: "src/ftbqkeys/kubejs/assets/kubejs/lang/en_us.json",
+    target: "kubejs/assets/kubejs/lang/en_us.json",
+  },
+];
 
 // 创建一个文件以写入压缩包数据
 const output = fs.createWriteStream("dist/package.zip");
 const archive = archiver("zip", {
   zlib: { level: 9 }, // 设置压缩级别
 });
+
+// 下面开始处理zh_cn.json
+// 读取zh_cn.json, en_us.json , auto.json
+const zh_cn = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, "ftbqkeys/kubejs/assets/kubejs/lang/zh_cn.json"),
+    "utf-8"
+  )
+);
+const en_us = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, "ftbqkeys/kubejs/assets/kubejs/lang/en_us.json"),
+    "utf-8"
+  )
+);
+const auto = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, "ftbqkeys/kubejs/assets/kubejs/lang/auto.json"),
+    "utf-8"
+  )
+);
+// 合并zh_cn.json与en_us.json
+for (let key in en_us) {
+  if (!/^[\s]*$/.test(zh_cn[key])) continue; // zh_cn已有非空内容
+  let value = en_us[key]; // 原文内容
+  if (key in auto) value += "\n\n" + auto[key]; // 自动补充机翻内容
+  zh_cn[key] = value; // 修改zh_cn.json
+}
 
 output.on("close", function () {
   const byteSize = archive.pointer();
@@ -46,6 +88,15 @@ archive.pipe(output);
 directories.forEach((directory) => {
   archive.directory(directory.path, directory.target);
 });
+
+// 遍历数组并添加每个文件到压缩包
+files.forEach((file) => {
+  archive.file(file.file, { name: file.target });
+});
+
+// 添加合并完成的zh_cn.json到压缩包
+const zhCnContent = JSON.stringify(zh_cn, null, 2);
+archive.append(zhCnContent, { name: "kubejs/assets/kubejs/lang/zh_cn.json" });
 
 // 完成文件添加后，关闭压缩包
 archive.finalize();
